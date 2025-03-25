d8d4d530e75889e3e17ee02a4152020ea9b63331
Sivakarthikeyan Lakshmanan <85480409+SivaKarthi1@users.noreply.github.com>
2025-03-25 14:52:15 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 function connectToDatabase() {
   const connection = mysql.createConnection({
-    host: 'localhost', 
+    host: '127.0.0.1', 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME 
